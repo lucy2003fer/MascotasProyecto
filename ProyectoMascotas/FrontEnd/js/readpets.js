a5798dacd3b2224da3d25b4baebe74d63aa5e178
lucy2003fer <lucy48fernada@gmail.com>
2025-05-11 20:53:38 -0500

    implemente el nuevo mapeo aquí

--- ProyectoMascotas/FrontEnd/js/readpets.js
+++ ProyectoMascotas/FrontEnd/js/readpets.js
@@ -1,28 +1,24 @@
 document.addEventListener("DOMContentLoaded", async () => {
-
-  // Verificar si el token está presente
-  checkToken();  // Esto llama a la función checkToken() de token.js
+  checkToken();
 
   document.getElementById("btn-back").addEventListener("click", () => window.location.href = "/ProyectoMascotas/FrontEnd/pets.html");
   document.getElementById("btn-close").addEventListener("click", () => window.location.href = "/ProyectoMascotas/FrontEnd/pets.html");
 
-  // Obtener ID de la mascota desde la URL
   const petId = new URLSearchParams(window.location.search).get("id");
   if (!petId) return console.log("ID de mascota no proporcionado.");
 
   try {
-    // Obtener los datos de la mascota
     const response = await fetch(`${API_URL}/api/pets/${petId}`);
     if (!response.ok) throw new Error("Error al obtener la mascota.");
 
     const pet = await response.json();
 
-    // Asignar los datos a los elementos HTML
+
     document.getElementById("pet-image").src = pet.photo ? `${API_URL}${pet.photo}` : 'imgs/default-pet.png';
     document.getElementById("pet-name").textContent = pet.name || "Sin nombre";
-    document.getElementById("pet-race").textContent = pet.race_id || "Sin raza";
-    document.getElementById("pet-category").textContent = pet.category_id || "Sin categoría";
-    document.getElementById("pet-gender").textContent = pet.gender_id || "Sin género";
+    document.getElementById("pet-race").textContent = pet.races?.name || "Sin raza";
+    document.getElementById("pet-category").textContent = pet.category?.name || "Sin categoría";
+    document.getElementById("pet-gender").textContent = pet.gender?.name || "Sin género";
   } catch (error) {
     console.error(error);
     console.log("No se pudo cargar la información de la mascota.");
